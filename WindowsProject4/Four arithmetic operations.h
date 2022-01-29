89e82077c21103023ec8439641b1284f60980d0f
Candylumiele <Candylumiele@gmail.com>
2022-01-29 18:23:34 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsProject4/Four arithmetic operations.h
@@ -0,0 +1,32 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+//‘«‚µŽZ
+double plus(double x,double y)
+{
+	double ans = 0;
+	ans = x + y;
+	return ans;
+}
+//ˆø‚«ŽZ
+double sub(double x, double y)
+{
+	double ans = 0;
+	ans = x - y;
+	return ans;
+}
+//Š|‚¯ŽZ
+double mult(double x, double y)
+{
+	double ans = 0;
+	ans = x * y;
+	return ans;
+}
+//Š„‚èŽZ
+double div(double x, double y)
+{
+	double ans = 0;
+	ans = x / y;
+	return ans;
+}
---
+++ WindowsProject4/WindowsProject4.cpp
@@ -0,0 +1,6 @@
+﻿#include<windows.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+#include"Four arithmetic operations.h"
