a73fda9e51cbe7f7bef92b1396a7fc520b6c44d5
Candylumiele <Candylumiele@gmail.com>
2022-01-31 19:53:08 +0900

    beta

--- WindowsProject4/WinMain.cpp
+++ WindowsProject4/WinMain.cpp
@@ -12,3 +12,26 @@
 #include"mathandPhysicsdefine.h"
 //定義
 #define _USE_MATH_DEFINES
+//Main関数
+int WINAPI WinMain
+(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow
+)
+{
+	HWND hwnd = CreateWindow(
+		TEXT("STATIC"), TEXT("culc"),
+		WS_CAPTION,
+		100, 100, 200, 200, NULL, NULL,
+		hInstance, NULL
+	);
+
+	if (hwnd == NULL) return 0;
+
+	ShowWindow(hwnd, SW_SHOW);
+	MessageBox(NULL, TEXT("culc"),
+		TEXT("hello"), MB_ICONINFORMATION);
+	return 0;
+}
